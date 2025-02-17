628b55d7ed0d4b53d367e26ee327fd1f0867a444
Fadi Touza <faditouza@icloud.com>
2025-02-17 15:17:35 +0100

    Server bug fix

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     connectMongoDB();
   });
